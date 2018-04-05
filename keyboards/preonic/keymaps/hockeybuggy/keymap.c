5e41b43da31c058be0aefd558a88d85d84e83879
Douglas Anderson <hockeybuggy@gmail.com>
2018-04-05 00:04:32 -0400

    Fix the emoji, but not seeing it work on GNULinux.

--- keyboards/preonic/keymaps/hockeybuggy/keymap.c
+++ keyboards/preonic/keymaps/hockeybuggy/keymap.c
@@ -58,8 +58,6 @@ const uint32_t PROGMEM unicode_map[] = {
     [EM_THUMBS_DOWN] = 0x1F44E,
     [EM_CURLING] = 0x1F94C,
 };
-#define EM_PENGY   X(PENGUIN)
-#define EM_BOARY   X(BOAR)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -78,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = {
   {KC_GRV,               KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC},
-  {KC_TAB,               KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL},
+  {KC_TAB,               KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS},
   {MT(MOD_LCTL, KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LSFT,              KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
   {KC_LCTL,              KC_LGUI, KC_LALT, _______, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
