aa877f801cf7ad7d1d29b4db827424518b92f59f
Douglas Anderson <hockeybuggy@gmail.com>
2018-06-06 19:08:43 -0400

    Switching the dash and equals keys.
    After using the keyboard for a week I wasn't happy that the raise key
    would be the opposite of the behaviour of the qwerty row's "raise to get
    the upper symbol on the key.

--- keyboards/preonic/keymaps/hockeybuggy/keymap.c
+++ keyboards/preonic/keymaps/hockeybuggy/keymap.c
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |  \   |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |Smile |Laugh |Think |TmbsUp|Metal |Emoji |   _  |   +  |   {  |   }  |  |   |
+ * |      |Smile |Laugh |Think |TmbsUp|Metal |Emoji |   -  |   =  |   {  |   }  |  |   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |Frown | Sad  |Spark |TmbsDn| Fire | LGTM |      |      | Home | End  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -86,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = {
   {KC_TILD, KC_EXLM,  KC_AT,    KC_HASH,     KC_DLR,         KC_PERC,    KC_CIRC,           KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL},
   {KC_GRV,  KC_1,     KC_2,     KC_3,        KC_4,           KC_5,       KC_6,              KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS},
-  {_______, EM_SMILE, EM_LAUGH, EM_THINKING, EM_THUMBS_UP,   EM_METAL,   MACRO_EM_KEYBOARD, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE},
+  {_______, EM_SMILE, EM_LAUGH, EM_THINKING, EM_THUMBS_UP,   EM_METAL,   MACRO_EM_KEYBOARD, KC_MINS, KC_EQL,  KC_LCBR, KC_RCBR, KC_PIPE},
   {_______, EM_FROWN, EM_SAD,   EM_SPARKLES, EM_THUMBS_DOWN, EM_FIRE,    MACRO_LGTM,        _______, _______, KC_HOME, KC_END,  _______},
   {_______, _______,  _______,  _______,     _______,        _______,    _______,           _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
 },
@@ -97,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |  \   |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
+ * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   [  |   ]  |  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      | PgDn | PgUp |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -107,7 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = {
   {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL},
   {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSLS},
-  {_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS},
+  {_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_BSLS},
   {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_PGUP, KC_PGDN, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
 },
