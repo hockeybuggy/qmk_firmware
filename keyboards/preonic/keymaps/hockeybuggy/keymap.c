cba3b79ac80a40d972dc897f03b38f4b4b0dcc99
Douglas Anderson <hockeybuggy@gmail.com>
2019-03-31 18:47:33 -0400

    Updating my preonic keyboard layout.
    I am moving the alt key over. Removing the emojii keyboard macro that
    doesn't really work.
    
    I also generally made more pass-throughs for layer keys.

--- keyboards/preonic/keymaps/hockeybuggy/keymap.c
+++ keyboards/preonic/keymaps/hockeybuggy/keymap.c
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | GUI  | Alt  |      |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+ * | Ctrl | GUI  |      |  Alt |Lower |    Space    |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
@@ -66,17 +66,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,               KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS},
   {MT(MOD_LCTL, KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LSFT,              KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
-  {KC_LCTL,              KC_LGUI, KC_LALT, _______, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+  {KC_LCTL,              KC_LGUI, _______, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
 
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
- * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Del  |
+ * |   `  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |  \   |
+ * |      |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |Smile |Laugh |Think |TmbsUp|Metal |Emoji |   -  |   =  |   [  |   ]  |  |   |
+ * |      |Smile |Laugh |Think |TmbsUp|Metal |      |   -  |   =  |   [  |   ]  |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |Frown | Sad  |Spark |TmbsDn| Fire | LGTM |      |      | Home | End  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -84,20 +84,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `----------------------------------------------------------------------------------'
  */
 [_LOWER] = {
-  {KC_TILD, KC_EXLM,  KC_AT,    KC_HASH,     KC_DLR,         KC_PERC,    KC_CIRC,           KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL},
-  {KC_GRV,  KC_1,     KC_2,     KC_3,        KC_4,           KC_5,       KC_6,              KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS},
-  {_______, EM_SMILE, EM_LAUGH, EM_THINKING, EM_THUMBS_UP,   EM_METAL,   MACRO_EM_KEYBOARD, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,  KC_PIPE},
-  {_______, EM_FROWN, EM_SAD,   EM_SPARKLES, EM_THUMBS_DOWN, EM_FIRE,    MACRO_LGTM,        _______, _______, KC_HOME, KC_END,  _______},
-  {_______, _______,  _______,  _______,     _______,        _______,    _______,           _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
+  {KC_GRV,  KC_EXLM,  KC_AT,    KC_HASH,     KC_DLR,         KC_PERC,    KC_CIRC,    KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL},
+  {_______, KC_1,     KC_2,     KC_3,        KC_4,           KC_5,       KC_6,       KC_7,    KC_8,    KC_9,    KC_0,    _______},
+  {_______, EM_SMILE, EM_LAUGH, EM_THINKING, EM_THUMBS_UP,   EM_METAL,   _______,    KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______},
+  {_______, EM_FROWN, EM_SAD,   EM_SPARKLES, EM_THUMBS_DOWN, EM_FIRE,    MACRO_LGTM, _______, _______, KC_HOME, KC_END,  _______},
+  {_______, _______,  _______,  _______,     _______,        _______,    _______,    _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
 },
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |  \   |
+ * |      |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  \   |
+ * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      | PgDn | PgUp |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -106,8 +106,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = {
   {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL},
-  {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSLS},
-  {_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_BSLS},
+  {_______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______},
+  {_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______},
   {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_PGDN, KC_PGUP, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
 },
@@ -211,9 +211,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             SEND_STRING(":metal:");
             return false;
 
-          case MACRO_EM_KEYBOARD:
-            /* SEND_STRING(SS_DOWN(X_LGUI)SS_DOWN(X_LCTRL)" "SS_UP(X_LGUI)SS_UP(X_LCTRL)); */
-            return false;
           case MACRO_LGTM:
             SEND_STRING(":thumbsup: Looks good to me :sparkles:");
             return false;
