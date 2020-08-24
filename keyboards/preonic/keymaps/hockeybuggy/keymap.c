ac94b5c1af792056052a75b1a0e3db501bc9ad50
Douglas Anderson <hockeybuggy@gmail.com>
2020-08-23 20:54:24 -0400

    Add Combos.
    I ordered a new keyboard today. To prepare for it this commit adds a few
    combos. Now I am able to chord keys and get my keyboards to emit some of
    the keys that were slighty awkward under layers.

--- keyboards/preonic/keymaps/hockeybuggy/keymap.c
+++ keyboards/preonic/keymaps/hockeybuggy/keymap.c
@@ -45,6 +45,7 @@ enum preonic_keycodes {
 };
 
 
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
@@ -134,6 +135,61 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 };
+
+
+/* Combo Map
+  ,-----------------------------------------------------------------------------------.
+ * |                                                                                   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |             {      }                           _      +                           |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |             (      )                           -      =                           |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |             [      ]                                                              |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |                                                                                   |
+ * `-----------------------------------------------------------------------------------'
+ */
+enum combos {
+  // Parens
+  WE_LCBR,
+  ER_RCBR,
+  SD_LPRN,
+  DF_RPRN,
+  XC_LBRC,
+  CV_RBRC,
+  // Other Symbols
+  HJ_MINS,
+  JK_EQL,
+  YU_UNDS,
+  UI_PLUS,
+};
+
+const uint16_t PROGMEM we_combo[] = {KC_W, KC_E, COMBO_END};
+const uint16_t PROGMEM er_combo[] = {KC_E, KC_R, COMBO_END};
+const uint16_t PROGMEM sd_combo[] = {KC_S, KC_D, COMBO_END};
+const uint16_t PROGMEM df_combo[] = {KC_D, KC_F, COMBO_END};
+const uint16_t PROGMEM xc_combo[] = {KC_X, KC_C, COMBO_END};
+const uint16_t PROGMEM cv_combo[] = {KC_C, KC_V, COMBO_END};
+const uint16_t PROGMEM hj_combo[] = {KC_H, KC_J, COMBO_END};
+const uint16_t PROGMEM jk_combo[] = {KC_J, KC_K, COMBO_END};
+const uint16_t PROGMEM yu_combo[] = {KC_Y, KC_U, COMBO_END};
+const uint16_t PROGMEM ui_combo[] = {KC_U, KC_I, COMBO_END};
+
+combo_t key_combos[COMBO_COUNT] = {
+  [WE_LCBR] = COMBO(we_combo, KC_LCBR),
+  [ER_RCBR] = COMBO(er_combo, KC_RCBR),
+  [SD_LPRN] = COMBO(sd_combo, KC_LPRN),
+  [DF_RPRN] = COMBO(df_combo, KC_RPRN),
+  [XC_LBRC] = COMBO(xc_combo, KC_LBRC),
+  [CV_RBRC] = COMBO(cv_combo, KC_RBRC),
+  [HJ_MINS] = COMBO(hj_combo, KC_MINS),
+  [JK_EQL] = COMBO(jk_combo, KC_EQL),
+  [YU_UNDS] = COMBO(yu_combo, KC_UNDS),
+  [UI_PLUS] = COMBO(ui_combo, KC_PLUS),
+};
+
+
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
